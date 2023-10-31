c99fda1df15648566457c4a651f112e7a6eb9966
geversonfernandes <gever.fernandes@gmail.com>
2023-10-31 07:54:59 -0300

    Meals route created

---
+++ src/routes/meals.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+import { randomUUID } from 'crypto'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post<{ Params: { user_id: string } }>(
+    '/meals/:user_id',
+    async (request, reply) => {
+      const newMealsSchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isDiet: z.boolean(),
+      })
+
+      const { name, description, isDiet } = newMealsSchema.parse(request.body)
+
+      const userId = request.params.user_id
+
+      console.log(userId)
+
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        isDiet,
+        user_id: userId,
+      })
+
+      return reply.status(201).send('Refeição criada com sucesso')
+    },
+  )
+}
